3fc8da90ff3b0d9995310099bc7999206e9eab29
TheLuckyLucker <conni.blach@web.de>
2016-06-10 14:19:12 +0200

    Added .isCanceled() to the AckermanNumbers class, such that computation is canceled when the cancel button is pressed

--- ackermanthreads/AckermanNumbers.java
+++ ackermanthreads/AckermanNumbers.java
@@ -5,60 +5,88 @@
  */
 package ackermanthreads;
 
+import javafx.concurrent.Task;
+
 /**
  *
  * @author Conny Blach, s4329872
  * @author Tiko Huizinga, s4460898
  */
-public class AckermanNumbers{
+public class AckermanNumbers {
+
     private int ackerman;
-    private int m,n;
+    private int m, n;
     private boolean canceled;       // This was used for task 2
-    
-    public AckermanNumbers(int m, int n){
+
+    public AckermanNumbers(int m, int n) {
         this.m = m;
         this.n = n;
         canceled = false;
     }
-    
+
     /**
      * Calculates the ackermannfunction for m and n
+     * If the task.isCancelled(), the computation stops
      * @param m
      * @param n
-     * @return 
+     * @param t The task which is calculating the ackerman number
+     * @return
      */
-    public int calculate(int m, int n){
-        if(m == 0){
+    public int calculate(int m, int n, Task<Integer> t) {
+        if (t.isCancelled()) {
+            System.out.println("canceled");
+            this.ackerman = 0;
+            return 0;
+        } else if (m == 0) {
             this.ackerman = n + 1;
             return n + 1;
+        } else if (m > 0 && n == 0) {
+            return calculate(m - 1, 1, t);
+        } else if (m > 0 && n > 0) {
+            return calculate(m - 1, calculate(m, n - 1, t), t);
         }
-        else if(m > 0 && n == 0){
-            return calculate(m-1, 1);
-        }
-        else if(m > 0 && n > 0){
-            return calculate(m-1, calculate(m, n-1));
-        }
-        this.ackerman = 0 ;
+        this.ackerman = 0;
         return 0;
     }
-    
-    public int getAckerman(){
+
+    /**
+     * 
+     * @return Ackerman number
+     */
+    public int getAckerman() {
         return this.ackerman;
     }
-    
-    public int getN(){
+
+    /**
+     * 
+     * @return the input n
+     */
+    public int getN() {
         return this.n;
     }
-    
-    public int getM(){
+
+    /**
+     * 
+     * @return the input m
+     */
+    public int getM() {
         return this.m;
     }
-    
-    public void setCanceled(boolean value){
+
+    /**
+     * Set the canceled value to true. 
+     * This was used for task 2
+     * @param value 
+     */
+    public void setCanceled(boolean value) {
         this.canceled = value;
     }
-    
-    public boolean getCanceled(){
+
+    /**
+     * This was used for task2
+     * @return the boolean value of canceled
+     */
+    public boolean getCanceled() {
         return this.canceled;
     }
 }
--- ackermanthreads/AckermanRunner.java
+++ ackermanthreads/AckermanRunner.java
@@ -8,52 +8,48 @@ package ackermanthreads;
 import javafx.application.Platform;
 import javafx.concurrent.Task;
 
-
 /**
  *
  * @author Conny Blach, s4329872
  * @author Tiko Huizinga, s4460898
  */
-public class AckermanRunner extends Task<Integer>{
+public class AckermanRunner extends Task<Integer> {
 
     private AckermanNumbers numbers;
     private final FXMLDocumentController controller;
-    
-    public AckermanRunner(AckermanNumbers A, FXMLDocumentController t){
+
+    public AckermanRunner(AckermanNumbers A, FXMLDocumentController t) {
         this.numbers = A;
         this.controller = t;
     }
-    
-    /**
-     * This was used for task2 when we had to use the threads
-    @Override
-    public void run() {
-        numbers.calculate(numbers.getM(), numbers.getN());
-        Platform.runLater(() -> controller.updateResult(numbers.getAckerman()));
-        if(numbers.getCanceled()){
-            Platform.runLater(() -> controller.setStatusMessage("Canceled"));
-        }
-        else{
-            Platform.runLater(() -> controller.setStatusMessage("Finished"));
-        }
-    }*/
 
     /**
+     * This was used for task2 when we had to use the threads
+     *
+     * @Override public void run() { numbers.calculate(numbers.getM(),
+     * numbers.getN()); Platform.runLater(() ->
+     * controller.updateResult(numbers.getAckerman()));
+     * if(numbers.getCanceled()){ Platform.runLater(() ->
+     * controller.setStatusMessage("Canceled")); } else{ Platform.runLater(() ->
+     * controller.setStatusMessage("Finished")); }
+    }
+     */
+    /**
      * Call function which is executed by the Ackermann Worker
+     *
      * @return the new value for value of Task
-     * @throws Exception 
+     * @throws Exception
      */
     @Override
     protected Integer call() throws Exception {
-        numbers.calculate(numbers.getM(), numbers.getN());
+        numbers.calculate(numbers.getM(), numbers.getN(), this);
         Platform.runLater(() -> controller.updateResult(numbers.getAckerman()));
-        if(this.isCancelled()){
+        if (this.isCancelled()) {
             Platform.runLater(() -> controller.setStatusMessage("Canceled"));
-        }
-        else{
+        } else {
             Platform.runLater(() -> controller.setStatusMessage("Finished"));
         }
         return 0;
     }
-    
+
 }
